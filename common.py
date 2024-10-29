7e4f9e9abbc03e9857179888bc06fc6b1081c0a1
Andrea Brogi <andrea.brogi@etu.minesparis.psl.eu>
2024-10-29 16:00:24 +0100

    Dropna

--- common.py
+++ common.py
@@ -3,6 +3,7 @@ DATA_PATH_NAME = "DataForTable2.1WHR2023.xls"
 
 def load_data(filename = DATA_PATH_NAME):
     df = pd.read_excel(filename)
+    df.dropna(inplace = True)
     return df
 
 def normalize(df):
